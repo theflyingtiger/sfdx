f225c83571c1caab226e0b63f4ecc9be22854bc4
The Flying Tiger <liuning66@hotmail.com>
2021-04-18 23:52:18 -0700

    26 public boolean

--- force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
+++ force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
@@ -1,10 +1,32 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class BmiCalculator extends LightningElement {
     cardTitle = 'BMI Calculator';
 
-    changePrivatePropertyHandler(){
-        this.cardTitle = 'Change Value';
-        console.log('value:', this.cardTitle);
+    weight;
+    height;
+
+    @track bmi;
+    onWeightChange(event){
+        this.weight = parseFloat(event.target.value);
+    }
+
+    onHeightChange(event){
+        this.height = parseFloat(event.target.value);
+    }
+
+    calculateBMI(){
+        try{
+        this.bmi = this.weight/(this.height*this.height);
+        } catch(error){
+            this.bmi = undefined;
+        }
+    }
+
+    get bmiValue(){
+        if(this.bmi === undefined){
+            return "";
+        }
+        return `Your BMI is: ${this.bmi}`;
     }
 }
